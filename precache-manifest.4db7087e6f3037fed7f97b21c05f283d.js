128862fa4d31c527a111b5eac0acc75e84ca1ab5
Rainspistols <ffly@mail.ru>
2020-04-28 02:22:55 +0200

    Updates

---
+++ precache-manifest.4db7087e6f3037fed7f97b21c05f283d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "769d7978a6326c767313f61b5b7c5736",
+    "url": "/COVID-19-Tracker-Application/index.html"
+  },
+  {
+    "revision": "92befcceb1e1d7652377",
+    "url": "/COVID-19-Tracker-Application/static/css/main.81ecdbe0.chunk.css"
+  },
+  {
+    "revision": "97aa0d70130e04e1a76c",
+    "url": "/COVID-19-Tracker-Application/static/js/2.38e8efe7.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/COVID-19-Tracker-Application/static/js/2.38e8efe7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92befcceb1e1d7652377",
+    "url": "/COVID-19-Tracker-Application/static/js/main.c1be61ea.chunk.js"
+  },
+  {
+    "revision": "0565707cb0617dc154a7",
+    "url": "/COVID-19-Tracker-Application/static/js/runtime-main.51aebc4e.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/COVID-19-Tracker-Application/static/media/covid19.d7265326.png"
+  }
+]);
